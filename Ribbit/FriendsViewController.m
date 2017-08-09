1d1c06a63a6de73b250baab28fb782604a3d7fc4
MrLotU <j.koopman@jarict.nl>
2017-08-09 13:53:09 +0200

    Fixed bug 1 and 2

--- Ribbit/FriendsViewController.m
+++ Ribbit/FriendsViewController.m
@@ -40,7 +40,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return [self.friends count];
+    return [self.friends count]; 
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
